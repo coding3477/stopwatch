2d94819f131e865f751a05c4653d27304b2f84e3
coding3477 <jgap3477@naver.com>
2021-01-17 11:11:08 +0900

    implement button

--- stopwatch.js
+++ stopwatch.js
@@ -1,8 +1,10 @@
 let hh = 0;
 let mm = 0;
 let ss = 0;
+let timer = null;
+let state = "stopped";
 
-window.setInterval(IntervalHandler, 1000);
+//window.setInterval(IntervalHandler, 1000);
 
 function IntervalHandler() {
     ss++;
@@ -62,6 +64,30 @@ function IntervalHandler() {
     document.getElementById("ss").textContent = fillZero(2, ss.toString());
 }
 
-function fillZero(width, str){
-    return str.length >= width ? str:new Array(width-str.length+1).join('0')+str;
+function fillZero(width, str) {
+    return str.length >= width ? str : new Array(width - str.length + 1).join('0') + str;
 }
+
+document.getElementById("start_stop").addEventListener("click", function () {
+    if (state == "stopped") {
+        timer = window.setInterval(IntervalHandler, 1000);
+        state = "started";
+        this.innerText = "Stop";
+    } else {
+        window.clearInterval(timer);
+        state = "stopped";
+        timer = null;
+        this.innerText = "Start";
+    }
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+    window.clearInterval(timer);
+    state = "stopped";
+    timer = null;
+    document.getElementById("start_stop").innerText = "Start";
+
+    document.getElementById("hh").textContent = "00";
+    document.getElementById("mm").textContent = "00";
+    document.getElementById("ss").textContent = "00";
+});
